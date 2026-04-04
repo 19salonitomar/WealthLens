efa9fb311a85e590e0e9bbfaf38f86cd817c01a9
Saloni Tomar <tomarsaloni19@gmail.com>
2026-04-04 18:04:39 +0530

    Implemented Header Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 // import { Toaster } from "@/components/ui/sonner"
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><Header />
         {children}
         {/*<Toaster />*/}
       </body>
