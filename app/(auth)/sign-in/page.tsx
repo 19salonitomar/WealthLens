63c4995787c39ceccfcbf2de2a6af51c44dc923d
Saloni Tomar <tomarsaloni19@gmail.com>
2026-04-06 21:50:21 +0530

    Update Code & Make CodeRabbit Happy!

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -48,7 +48,7 @@ const SignIn = () => {
                 <InputField
                     name="email"
                     label="Email"
-                    placeholder="contact@jsmastery.com"
+                    placeholder="contact@signalist.com"
                     register={register}
                     error={errors.email}
                     validation={{
