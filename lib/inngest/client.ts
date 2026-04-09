3d52cbf74cb24e655a28f3b109ff0034b15016ff
Saloni Tomar <tomarsaloni19@gmail.com>
2026-04-09 18:38:48 +0530

    Added Correct Project ID

--- lib/inngest/client.ts
+++ lib/inngest/client.ts
@@ -1,6 +1,6 @@
-import { Inngest} from "inngest";
+import { Inngest } from "inngest";
 
 export const inngest = new Inngest({
-    id: 'wealthlens',
-    ai: { gemini: { apiKey: process.env.GEMINI_API_KEY! }}
-})
+    id: "wealth-lens",
+    ai: { gemini: { apiKey: process.env.GEMINI_API_KEY! } }
+});
